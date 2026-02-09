d6377796cb06338bfc8ad7ae5b2153f969f59745
noeljabraham <noeljabraham@gmail.com>
2026-02-09 13:00:25 -0500

    Add bruma-release package skeleton and welcome file

---
+++ bruma-release-1.0/debian/upstream.ex/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/bruma-release/issues
+# Bug-Submit: https://github.com/<user>/bruma-release/issues/new
+# Changelog: https://github.com/<user>/bruma-release/blob/master/CHANGES
+# Documentation: https://github.com/<user>/bruma-release/wiki
+# Repository-Browse: https://github.com/<user>/bruma-release
+# Repository: https://github.com/<user>/bruma-release.git
