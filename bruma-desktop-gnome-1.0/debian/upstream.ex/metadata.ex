509f249e37cee8bc75adac555e29d1a35e2fbe13
noeljabraham <noeljabraham@gmail.com>
2026-02-09 21:44:34 -0500

    Add GNOME and KDE desktop metapackages

---
+++ bruma-desktop-gnome-1.0/debian/upstream.ex/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/bruma-desktop-gnome/issues
+# Bug-Submit: https://github.com/<user>/bruma-desktop-gnome/issues/new
+# Changelog: https://github.com/<user>/bruma-desktop-gnome/blob/master/CHANGES
+# Documentation: https://github.com/<user>/bruma-desktop-gnome/wiki
+# Repository-Browse: https://github.com/<user>/bruma-desktop-gnome
+# Repository: https://github.com/<user>/bruma-desktop-gnome.git
---
+++ bruma-desktop-kde-1.0/debian/upstream.ex/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/bruma-desktop-kde/issues
+# Bug-Submit: https://github.com/<user>/bruma-desktop-kde/issues/new
+# Changelog: https://github.com/<user>/bruma-desktop-kde/blob/master/CHANGES
+# Documentation: https://github.com/<user>/bruma-desktop-kde/wiki
+# Repository-Browse: https://github.com/<user>/bruma-desktop-kde
+# Repository: https://github.com/<user>/bruma-desktop-kde.git
